d1b7c2a27ac00c1906b16e168556cb3979de2d7d
Janhavi <kulkarnijanhavi0906@gmail.com>
2023-11-17 20:59:27 +0530

    Render songs from Song objects

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/songs', function () {
+    return "Songs";
+});
+
+
+Route::get('/songs', function () {
+    return view('songs');
+});
+use practicals\Song;
+
+Route::get('/songs', function () {
+    $song = new Song();
+    $song->setTitle('With You');
+    return view('songs', ['song' => $song]);
+});
+Route::get('/songs', function () {
+  $song1 = new Song();
+  $song1->setTitle("Stan");
+  $song1->setArtist("Eminem");
+
+  $song2 = new Song();
+  $song2->setTitle("Nothing Else Matters");
+  $song2->setArtist("Metallica");
+
+  $song3 = new Song();
+  $song3->setTitle("With You");
+  $song3->setArtist("A P Dhillon");
+
+  return view('songs', ['song' => $song, 'songs' => $songs]);
+});
+
+Route::get('/veggies/baigan', function () {
+    return 'Baigan';
+});
+Route::get('/veggies/{veggieName}', function (string $veggiename) {
+        return $veggiename;
+}) ->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo','gobhi']);
+
